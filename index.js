0e168ec3bd5e618bce5ef75c424debc87345f912
Akhilchary <61978652+Akhilchary@users.noreply.github.com>
2021-10-07 18:35:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+$(function(){
+    $(".carousel-2 .previous").addClass("svg-caro-btn-Prev");
+    $(".carousel-2 .next").addClass("svg-caro-btn-Next");
+    $(".carousel-3 .previous").addClass("svg-caro-btn-Prev");
+    $(".carousel-3 .next").addClass("svg-caro-btn-Next");
+});
+
+$(".c2-img-1").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/863232-6132_2-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/863232-6132_1-494x593.jpeg");
+});
+
+$(".c2-img-2").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/602779-0083_2-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/602779-0083_1-494x593.jpeg");
+});
+
+$(".c2-img-3").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/278453-0012_2-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/278453-0012_1-494x593.jpeg");
+});
+
+$(".c2-img-4").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/271689-0001_3-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/271689-0001_1-494x593.jpeg");
+});
+
+$(".c2-img-5").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/601517-1936_2-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/601517-1936_1-494x593.jpeg");
+});
+
+$(".c2-img-6").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/06/hoodie_7_back-494x593.jpg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/06/hoodie_7_front-494x593.jpg");
+});
+
+$(".c2-img-7").mouseover(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/741227-0076_1-494x593.jpeg");
+}).mouseout(function(){
+    $(this).attr("src","https://fzi4k1gk2dw3t0fqy18sw8qi-wpengine.netdna-ssl.com/wp-content/uploads/2013/08/741227-0076_2-494x593.jpeg");
+});
+
+// $(".c2-img-8").mouseover(function(){
+//     $(this).attr("src","");
+// }).mouseout(function(){
+//     $(this).attr("src","");
+// });
+
